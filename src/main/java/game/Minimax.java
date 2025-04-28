c33eaa41bce79f6db90a4e5426f7cb87b06230f0
webbometry <131127417+webbometry@users.noreply.github.com>
2025-04-27 18:37:58 -0700

    a

--- src/main/java/game/Minimax.java
+++ src/main/java/game/Minimax.java
@@ -2,6 +2,16 @@ package game;
 
 public class Minimax {
     public String convert(String board) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int pos = 0; pos < board.length(); pos++) {
+            if (board.charAt(pos) == 'x') {
+                if (pos == 26) sb.append('.');
+                else sb.append('A' + pos);
+            } else if (board.charAt(pos) == 'o') {
+                if (pos == 26) sb.append(',');
+                else sb.append('a' + pos);
+            }
+        }
+        return sb.toString();
     }
 }
